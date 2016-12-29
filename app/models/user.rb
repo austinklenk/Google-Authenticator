c5e1128c635a1a77ae7c2a029dddec34eed122d9
Austin Klenk <austin@mastertechrv.com>
2016-12-28 20:58:11 -0500

    Initial Commit

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+	attr_accessor :gauth_token
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :google_authenticatable, :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
